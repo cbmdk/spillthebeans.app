5245d5e26fc9f070d0e8925ed991fdb957f34ccd
Christian <cbm@cbm.dk>
2020-05-24 01:43:01 +0200

    init

---
+++ Spillthebeans.api/Spillthebeans.api/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Spillthebeans.api.Services;
+using Spillthebeans.api.Views;
+
+namespace Spillthebeans.api
+{
+    public partial class App : Application
+    {
+        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
+        //To debug on Android emulators run the web backend against .NET Core not IIS
+        //If using other emulators besides stock Google images you may need to adjust the IP address
+        public static string AzureBackendUrl =
+            DeviceInfo.Platform == DevicePlatform.UWP ? "http://localhost:22908" : "http://localhost:22908";
+        public static bool UseMockDataStore = false;
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (UseMockDataStore)
+                DependencyService.Register<MockDataStore>();
+            else
+                DependencyService.Register<AzureDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
